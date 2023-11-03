a2c609120fffdd36ad15233dabdb09cd9f8402fd
Mary Webby <118230483+marywebby@users.noreply.github.com>
2023-11-03 18:29:33 +0000

    got movies displaying and the now playing tab

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'uri'
+require 'net/http'
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
+
+get("/now_playing") do
+  url = URI("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1")
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Get.new(url)
+  request["accept"] = "application/json"
+
+  request["Authorization"] = "Bearer #{ENV["MOVIE_DB_ACCESS_TOK"].to_s.gsub(/[\r\n]/, '')}"
+
+  response = http.request(request)
+  @now_playing = response.read_body
+
+  erb(:now_playing)
+end 
