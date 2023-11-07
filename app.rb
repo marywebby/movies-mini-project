288419b379227c485f329689c73840e1896b63b2
Mary Webby <118230483+marywebby@users.noreply.github.com>
2023-11-07 17:07:54 +0000

    trending tv and now playing done

--- app.rb
+++ app.rb
@@ -25,3 +25,21 @@ get("/now_playing") do
 
   erb(:now_playing)
 end 
+
+get("/trending_tv") do 
+  url = URI("https://api.themoviedb.org/3/trending/tv/day?language=en-US")
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Get.new(url)
+  request["accept"] = "application/json"
+  request["Authorization"] = "Bearer #{ENV["MOVIE_DB_ACCESS_TOK"].to_s.gsub(/[\r\n]/, '')}"
+
+  response = http.request(request)
+  body = JSON.parse(response.read_body)
+
+  @trending_tv = body["results"] 
+
+  erb(:trending_tv)
+end
